758d29c085544384be4a0f913aa5bfb66c677ff9
Amrapali Mohan Unkal <149380042+AmrapaliUnkal@users.noreply.github.com>
2025-02-14 09:26:57 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,16 @@
 #     GENAI_API_KEY: str = "AIzaSyBwzbuL30EsbczQb9rWyBFVGB9S2rKG5y4"
 # settings = Settings()
 
+# from decouple import config
+# class Settings:
+#     GENAI_API_KEY: str = config("GENAI_API_KEY")
+# settings = Settings()
+
 from decouple import config
+import os
+
 class Settings:
-    GENAI_API_KEY: str = config("GENAI_API_KEY")
+    GENAI_API_KEY: str = os.getenv("GENAI_API_KEY", config("GENAI_API_KEY", default=""))
+
 settings = Settings()
+
